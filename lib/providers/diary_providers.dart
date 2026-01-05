90db1e8362bc2c8dd8535d03eb93e0d7beb20e85
tomohiro ueno <hagurono9@gmail.com>
2026-01-05 21:14:46 +0900

    AIコメントなしに修正

--- lib/providers/diary_providers.dart
+++ lib/providers/diary_providers.dart
@@ -1,12 +1,9 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/diary.dart';
 import '../utils/database_helper.dart';
-import '../repositories/ai_repository.dart';
 
 // AsyncNotifier to handle list of diaries
 class DiaryListNotifier extends AsyncNotifier<List<Diary>> {
-  final _aiRepository = AiRepository();
-
   @override
   Future<List<Diary>> build() async {
     return _fetchAll();
@@ -19,11 +16,7 @@ class DiaryListNotifier extends AsyncNotifier<List<Diary>> {
   Future<void> addDiary(Diary diary) async {
     state = const AsyncValue.loading();
     state = await AsyncValue.guard(() async {
-      // Generate AI comment
-      final comment = await _aiRepository.generateComment(diary.content);
-      final diaryWithComment = diary.copyWith(aiComment: comment);
-
-      await DatabaseHelper.instance.create(diaryWithComment);
+      await DatabaseHelper.instance.create(diary);
       return _fetchAll();
     });
   }
